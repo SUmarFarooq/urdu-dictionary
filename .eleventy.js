59d5d059cb98501994f08064c7d7ff7a4f45f2b1
Umar Farooq <umar.farooq@salesforce.com>
2025-06-16 14:24:07 -0500

    11ty first draft

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/styles.css": "styles.css" });
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
